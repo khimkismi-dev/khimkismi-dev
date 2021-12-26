780e5b33cb1d1c55ddddb3e406aa3ab9c7c01d42
khimkismi-dev <email.report.bot@gmail.com>
2021-12-26 22:54:02 +0300

    another

--- func.py
+++ func.py
@@ -130,11 +130,12 @@ def echo(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup, parse_mode='HTML')
 
     elif re.search(r'save_badge_number#(\d+)', user.prev_msg):
-        contract_id = user.user_crm_info[user_id]['contract']['title']
+        contract_id = user.user_crm_info[user_id]['clean_data']['contract']
         badge_number = user.msg
         text = 'Вы точно хотите сохранить номер <b>%s</b> в качестве номера бирки для договора <b>%s</b>?' % \
                (badge_number, contract_id)
-        context.bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup, parse_mode='HTML')
+        Helpers.yes_no_menu(bot, chat_id, text)
+        # context.bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup, parse_mode='HTML')
 
 
 # ф-ия, обработчик нажатий inline-кнопок
